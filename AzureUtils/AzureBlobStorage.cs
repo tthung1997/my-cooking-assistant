045e489701ca66f86722cb84d5601a1ba9b0f1b7
Hung Tran <trieuhung.2210@gmail.com>
2020-04-19 19:33:31 -0700

    AzureUtils

---
+++ AzureUtils/AzureBlobStorage.cs
@@ -0,0 +1,120 @@
+﻿using Common;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureUtils
+{
+    public class AzureBlobStorage : IDataStorage
+    {
+        private readonly string loggingSource;
+        private readonly CloudBlobContainer container;
+        private readonly ILogger logger;
+        private readonly ICryptographer cryptographer;
+
+        public AzureBlobStorage(string connectionString, string containerName, ICryptographer cryptographer, ILogger logger, string loggingSource = "AzureBlobStorage")
+        {
+            this.logger = logger;
+            this.loggingSource = loggingSource;
+            this.cryptographer = cryptographer;
+            CloudStorageAccount storageAccount;
+            if (CloudStorageAccount.TryParse(connectionString, out storageAccount))
+            {
+                var client = storageAccount.CreateCloudBlobClient();
+                container = client.GetContainerReference(containerName);
+                container.CreateIfNotExistsAsync().Wait();
+            }
+            else
+            {
+                var ex = new ArgumentException("Failed to parse connection string to storage account");
+                logger.LogError(this.loggingSource, ex).Wait();
+                throw ex;
+            }
+        }
+
+        public async Task<bool> DeleteFile(string fileName)
+        {
+            try
+            {
+                if (await FileExists(fileName))
+                {
+                    await logger.LogInfo(loggingSource, $"File {fileName} exists. Start deleting.");
+                    return await container.GetBlobReference(fileName).DeleteIfExistsAsync();
+                }
+                else
+                {
+                    await logger.LogInfo(loggingSource, $"File {fileName} does not exist. No action is needed.");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                await logger.LogError(loggingSource, ex);
+                return false;
+            }
+        }
+
+        public async Task<bool> FileExists(string fileName)
+        {
+            try
+            {
+                return await container.GetBlobReference(fileName).ExistsAsync();
+            }
+            catch (Exception ex) 
+            {
+                await logger.LogError(loggingSource, ex);
+                return false;
+            }
+        }
+
+        public async Task<byte[]> DownloadFile(string fileName)
+        {
+            byte[] fileContent = new byte[0];
+            try
+            {
+                if (await FileExists(fileName))
+                {
+                    await logger.LogInfo(loggingSource, $"File {fileName} exists. Start downloading.");
+                    var blob = container.GetBlobReference(fileName);
+                    using (var ms = new MemoryStream())
+                    {
+                        await blob.DownloadToStreamAsync(ms);
+                        var encryptedContent = ms.ToArray();
+                        await logger.LogInfo(loggingSource, $"File {fileName} downloaded from blob storage. Length: {fileContent.Length} { (fileContent.Length > 1).ToPlural("byte") }");
+                        await logger.LogInfo(loggingSource, $"Decrypting file {fileName}.");
+                        fileContent = await cryptographer.AesDecrypt(encryptedContent);
+                        await logger.LogInfo(loggingSource, $"File {fileName} decrypted. Length: {fileContent.Length} { (fileContent.Length > 1).ToPlural("byte") }");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await logger.LogError(loggingSource, ex);
+            }
+            return fileContent;
+        }
+
+        public async Task<bool> UploadFile(string fileName, byte[] content)
+        {
+            try
+            {
+                await logger.LogInfo(loggingSource, $"Uploading file {fileName}");
+                var blob = container.GetBlockBlobReference(fileName);
+                await logger.LogInfo(loggingSource, $"Encrypting file {fileName} of {content.Length} { (content.Length > 1).ToPlural("byte") }");
+                await blob.UploadFromByteArrayAsync(content, 0, content.Length);
+                await logger.LogInfo(loggingSource, $"File {fileName} of {content.Length} { (content.Length > 1).ToPlural("byte") } uploaded to blob storage at {blob.Uri}");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                await logger.LogError(loggingSource, ex);
+                return false;
+            }
+        }
+    }
+}
---
+++ AzureUtils/KeyVaultAccessor.cs
@@ -0,0 +1,59 @@
+﻿using Common;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.Services.AppAuthentication;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AzureUtils
+{
+    public class KeyVaultAccessor : ICredentialAccessor
+    {
+        private const string keyVaultUrlRegexPattern = @"^https://[a-zA-Z0-9\-]+.vault.azure.net/?$";
+
+        private readonly KeyVaultClient keyVaultClient;
+        private readonly string keyVaultUrl;
+
+        private readonly string loggingSource;
+        private readonly ILogger logger;
+
+        public KeyVaultAccessor(string keyVaultUrl,
+            string loggingSource,
+            ILogger logger,
+            string azureServiceAuthConnectionString = "")
+        {
+            var azureServiceTokenProvider =
+                new AzureServiceTokenProvider(azureServiceAuthConnectionString);
+            keyVaultClient = new KeyVaultClient(
+                new KeyVaultClient.AuthenticationCallback(
+                    azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            var regex = new Regex(keyVaultUrlRegexPattern);
+            if (!regex.IsMatch(keyVaultUrl))
+            {
+                throw new ArgumentException(nameof(keyVaultUrl));
+            }
+            this.keyVaultUrl = keyVaultUrl;
+            this.logger = logger;
+            this.loggingSource = loggingSource;
+        }
+
+        public async Task<string> GetSecret(string key)
+        {
+            string secretValue = null;
+            try
+            {
+                await logger.LogInfo(loggingSource, $"Retrieving secret {key} from Key Vault {keyVaultUrl}");
+                var secretInKeyVault = await keyVaultClient.GetSecretAsync(keyVaultUrl, key);
+                await logger.LogInfo(loggingSource, $"Secret {key} loaded.");
+                secretValue = secretInKeyVault.Value;
+            }
+            catch (KeyVaultErrorException keyVaultException)
+            {
+                await logger.LogError(loggingSource, keyVaultException);
+            }
+            return secretValue;
+        }
+    }
+}
