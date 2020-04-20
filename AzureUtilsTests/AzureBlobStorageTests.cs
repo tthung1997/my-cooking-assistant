22116cc3f20c55b5081d3ed0472098ca758b73f8
Hung Tran <trieuhung.2210@gmail.com>
2020-04-19 19:36:42 -0700

    Initialized AzureUtilsTests and CoreTests

---
+++ AzureUtilsTests/AzureBlobStorageTests.cs
@@ -0,0 +1,45 @@
+using AzureUtils;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.CompilerServices;
+
+namespace AzureUtilsTests
+{
+    [TestClass]
+    public class AzureBlobStorageTests
+    {
+        private string storageConnectionString;
+        private ILogger logger;
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        
+        [TestInitialize]
+        public void Initialize()
+        {
+            logger = new ConsoleLogger();
+            storageConnectionString = GetTestParameter("storageConnectionString");
+        }
+
+        [TestMethod]
+        public void SuccessfullyCreateContainerTest()
+        {
+        }
+
+        private string GetTestParameter(string key)
+        {
+            return TestContext.Properties[key].ToString();
+        }
+    }
+}
