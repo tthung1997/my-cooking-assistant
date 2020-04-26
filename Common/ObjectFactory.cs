a6bbf240fff01e1057921e0c83b1ac614a1b975d
Hung Tran <trieuhung.2210@gmail.com>
2020-04-25 17:49:02 -0700

    ObjectFactory and ObjectFactoryTests

---
+++ Common/ObjectFactory.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace Common
+{
+    public class ObjectFactory
+    {
+        private static IDictionary<Guid, IDictionary<Type, object>> objectDictionary = new Dictionary<Guid, IDictionary<Type, object>>();
+        private static Guid? currentDomain = null;
+
+        public static void Initialize(Guid domainId, bool force = false) 
+        {
+            currentDomain = domainId;
+            if (!objectDictionary.ContainsKey(domainId) || force)
+            {
+                objectDictionary[domainId] = new Dictionary<Type, object>();
+            }
+        }
+
+        public static void Reset()
+        {
+            currentDomain = null;
+            objectDictionary = new Dictionary<Guid, IDictionary<Type, object>>();
+        }
+
+        public static void RegisterInstance<TInterface>(TInterface instance)
+        {
+            if (currentDomain == null || !objectDictionary.ContainsKey((Guid) currentDomain) || objectDictionary[(Guid) currentDomain] == null)
+            {
+                throw new InvalidOperationException("Please execute Initialize first");
+            }
+            objectDictionary[(Guid) currentDomain][typeof(TInterface)] = instance;
+        }
+
+        public static void RegisterInstance<TInterface>(Guid domainId, TInterface instance)
+        {
+            if (!objectDictionary.ContainsKey(domainId) || objectDictionary[domainId] == null)
+            {
+                throw new InvalidOperationException("Please execute Initialize with the specified domain first");
+            }
+            objectDictionary[domainId][typeof(TInterface)] = instance;
+        }
+
+        public static TInterface GetInstance<TInterface>()
+        {
+            if (currentDomain == null || !objectDictionary.ContainsKey((Guid)currentDomain) || objectDictionary[(Guid) currentDomain] == null)
+            {
+                throw new InvalidOperationException("Please execute Initialize first");
+            }
+            return (TInterface) objectDictionary[(Guid)currentDomain][typeof(TInterface)];
+        }
+
+        public static TInterface GetInstance<TInterface>(Guid domainId)
+        {
+            if (!objectDictionary.ContainsKey(domainId) || objectDictionary[domainId] == null)
+            {
+                throw new InvalidOperationException("Please execute Initialize with the specified domain first");
+            }
+            return (TInterface) objectDictionary[domainId][typeof(TInterface)];
+        }
+    }
+}
---
+++ CommonTests/ObjectFactoryTests.cs
@@ -0,0 +1,178 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonTests
+{
+    [TestClass]
+    public class ObjectFactoryTests
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            ObjectFactory.Reset();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+
+        }
+
+        [TestMethod]
+        [TestCategory("Bvt")]
+        public void SkipInitializationTest()
+        {
+            ILogger logger = new ConsoleLogger();
+            try
+            {
+                ObjectFactory.RegisterInstance<ILogger>(logger);
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Assert.AreEqual("Please execute Initialize first", ioe.Message);
+            }
+
+            try
+            {
+                ObjectFactory.GetInstance<ILogger>();
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Assert.AreEqual("Please execute Initialize first", ioe.Message);
+            }
+
+            try
+            {
+                ObjectFactory.RegisterInstance<ILogger>(Guid.Empty, logger);
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Assert.AreEqual("Please execute Initialize with the specified domain first", ioe.Message);
+            }
+
+            try
+            {
+                ObjectFactory.GetInstance<ILogger>(Guid.Empty);
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Assert.AreEqual("Please execute Initialize with the specified domain first", ioe.Message);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Bvt")]
+        public void FailToGetInstanceTest()
+        {
+            ObjectFactory.Initialize(Guid.Empty);
+            try
+            {
+                ObjectFactory.GetInstance<ILogger>();
+            }
+            catch (KeyNotFoundException knfe)
+            {
+                Assert.AreEqual($"The given key '{typeof(ILogger)}' was not present in the dictionary.", knfe.Message);
+            }
+
+            try
+            {
+                ObjectFactory.GetInstance<ILogger>(Guid.Empty);
+            }
+            catch (KeyNotFoundException knfe)
+            {
+                Assert.AreEqual($"The given key '{typeof(ILogger)}' was not present in the dictionary.", knfe.Message);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Bvt")]
+        public void SuccessfullyGetInstanceTest()
+        {
+            ObjectFactory.Initialize(Guid.Empty);
+            ILogger logger = new ConsoleLogger();
+            ObjectFactory.RegisterInstance<ILogger>(logger);
+            Assert.AreEqual(logger, ObjectFactory.GetInstance<ILogger>());
+
+            Guid newGuid = Guid.NewGuid();
+            ObjectFactory.Initialize(newGuid);
+            ILogger fileLogger = new FileLogger("log.txt");
+            ObjectFactory.RegisterInstance<ILogger>(newGuid, fileLogger);
+            Assert.AreEqual(fileLogger, ObjectFactory.GetInstance<ILogger>(newGuid));
+        }
+
+        [TestMethod]
+        [TestCategory("Bvt")]
+        public void InitializeWithoutForceTest()
+        {
+            ObjectFactory.Initialize(Guid.Empty);
+            ILogger logger = new ConsoleLogger();
+            ObjectFactory.RegisterInstance<ILogger>(logger);
+
+            ObjectFactory.Initialize(Guid.Empty);
+            Assert.AreEqual(logger, ObjectFactory.GetInstance<ILogger>());
+        }
+
+        [TestMethod]
+        [TestCategory("Bvt")]
+        public void InitializeWithForceTest()
+        {
+            ObjectFactory.Initialize(Guid.Empty);
+            ILogger logger = new ConsoleLogger();
+            ObjectFactory.RegisterInstance<ILogger>(logger);
+
+            ObjectFactory.Initialize(Guid.Empty, true);
+            try
+            {
+                ObjectFactory.GetInstance<ILogger>();
+            }
+            catch (KeyNotFoundException knfe)
+            {
+                Assert.AreEqual($"The given key '{typeof(ILogger)}' was not present in the dictionary.", knfe.Message);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Bvt")]
+        public void GetLatestInstanceTest()
+        {
+            ObjectFactory.Initialize(Guid.Empty);
+            ILogger consoleLogger = new ConsoleLogger();
+            ObjectFactory.RegisterInstance<ILogger>(consoleLogger);
+            ILogger fileLogger = new FileLogger("log.txt");
+            ObjectFactory.RegisterInstance<ILogger>(fileLogger);
+            Assert.AreEqual(fileLogger, ObjectFactory.GetInstance<ILogger>());
+        }
+
+        [TestMethod]
+        [TestCategory("Bvt")]
+        public void ResetTest()
+        {
+            ObjectFactory.Initialize(Guid.Empty);
+            ILogger logger = new ConsoleLogger();
+            ObjectFactory.RegisterInstance<ILogger>(logger);
+
+            ObjectFactory.Reset();
+
+            try
+            {
+                ObjectFactory.GetInstance<ILogger>();
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Assert.AreEqual("Please execute Initialize first", ioe.Message);
+            }
+
+            try
+            {
+                ObjectFactory.GetInstance<ILogger>(Guid.Empty);
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Assert.AreEqual("Please execute Initialize with the specified domain first", ioe.Message);
+            }
+        }
+    }
+}
